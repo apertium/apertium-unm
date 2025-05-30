a571b91b9bccfb83933e0bdc42b4228b7b6d6aec
Jonathan Washington <jonathan.n.washington@gmail.com>
2025-05-30 10:30:18 -0400

    more scraping script
    still needed:
    - vanilla bidix template
    - subcategory tags

--- dev/scrape-lexd.py
+++ dev/scrape-lexd.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import re
 
 lang1="unm"
 lang2="eng"
+reGloss = re.compile("\"(.*)\"")
+entryline = "<e><l>{}{}</l><r>{}{}</r></e>"
+tagtemplate = "<s n=\"{}\"/>"
 
 taglist = {
 	"AdjMorph": (["adj"], ["adj"]),
-	"Preverb-Lex": (["vaux"], ["vaux"]),
+	"Preverb-Lex": (["vaux"], ["vbmod"]),
 	"NounRoot-Anim": (["n", "aa"], ["n"]),
 	"NounRoot-Inan": (["n", "nn"], ["n"]),
 	"NounRoot-Family(2)": (["n", "nn"], ["n"]),
@@ -27,12 +31,41 @@ taglist = {
 }
 
 with open(os.path.join("../", "apertium-"+lang1+"."+lang1+".lexd"), 'r') as lexd:
+	tags = None
 	for lexdline in lexd.readlines():
-		line = lexdline.split("#")[0].strip()
-		if "LEXICON" in line:
-			lexname = line.split(" ")[1]
-			if lexname in taglist:
-				tags = taglist[lexname]
+		if "Dir/LR" not in lexdline:
+			line = lexdline.split("#")
+			uncommentedline = line[0].strip()
+
+			if "LEXICON" in uncommentedline:
+				lexname = uncommentedline.split(" ")[1]
+				if lexname in taglist:
+					tags = taglist[lexname]
+				else:
+					tags = None
+
+				#print(tags)
+			elif "PATTERN" in uncommentedline and "PATTERNS" not in uncommentedline:
+				tags = None
 			else:
-				print(lexname)
-			
+				if tags:
+					lang1tags = ""
+					lang2tags = ""
+					for tag in tags[0]:
+						lang1tags += tagtemplate.format((tag))
+					for tag in tags[1]:
+						lang2tags += tagtemplate.format((tag))
+					if ":" in lexdline:
+						lang1lemma = uncommentedline.split(':')[0]
+						lang1lemma = lang1lemma.replace("\\ ", "<b/>")
+						if len(line)>1:
+							result = reGloss.search(line[1].strip())
+							if result:
+								lang2lemmas = result.group(1)
+								for lemma in re.split("[,;]", lang2lemmas):
+									lemma = re.sub("\(.*?\)", "", lemma)
+									lemma = lemma.strip(' !')
+									lang2lemma = lemma.replace(" ", "<b/>")
+									line = entryline.format(lang1lemma, lang1tags, lang2lemma, lang2tags)
+									print(line)
+
